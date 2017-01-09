cbb312efee9165d5aa185cb32ee4a28cc9df0efc
Peter Ke <kepeterphysics@yahoo.com>
2017-01-09 22:20:42 +0000

    hello

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //Write your function calls below this line
+
+//Square
+setColor(2 , 0 , "purple");
+
+//4 Corners
+setColor(0 , 0 , "blue");
+setColor(0 , 9 , "blue");
+setColor(9 , 0 , "blue");
+setColor(9 , 9 , "blue");
+
+// Smiley face
+setColor(2 , 3 , "blue");
+setColor(2 , 6 , "blue");
+setColor(5 , 2 , "blue");
+setColor(6 , 3 , "blue");
+
+setColor(6 , 5 , "blue");
+setColor(6 , 4 , "blue");
+setColor(6 , 6 , "blue");
+setColor(5 , 7 , "blue");
+
